a491b4d3bb4ad1be8016eafb8d1fa05e4fc5995f
Katia MD <1719110962@utectulancingo.edu.mx>
2021-02-25 19:12:24 +0000

    app.py Prueba

--- insertar/json/app.py
+++ insertar/json/app.py
@@ -1,28 +1,29 @@
 import json
 
 class Json():
-  json_file = {}
 
-  def read(self):
-    with open("datos.json") as file:
-      self.json_file = json.load(file)
-    print(self.json_file)
+    json_file = {}
   
-  def write(self):
-    nombre = input ("Nombre: ")
-    email = input ("Email: ")
+    def read(self):
+        with open("datos.json") as file:
+            self.json_file = json.load(file)
+        print(self.json_file)
+  
+    def write(self):
+        nombre = input ("Nombre: ")
+        email = input ("Email: ")
+    
+        persona = {
+           "nombre":nombre,
+           "email":email
+        }
 
-    persona = {
-      "nombre":nombre,
-      "email":email
-    }
-
-    self.json_file["personas"].append(persona)
-    with open ("datos.json","w") as  file :
-      json.dumps(self.json_file,file)
+        self.json_file["personas"].append(persona)
+        with open("datos.json","w") as file:
+          json.dumps(self.json_file,file)
 
 
 objeto = Json()
-objeto.read
+objeto.read()
 objeto.write()
 print(objeto.json_file)
