c0289844a8c7d104ac77660de2db931ec6438b9f
Katia MD <1719110962@utectulancingo.edu.mx>
2021-02-25 20:31:28 +0000

    edades.py Creo

--- insertar/json/app.py
+++ insertar/json/app.py
@@ -1,29 +1,14 @@
 import json
 
 class Json():
-
-    json_file = {}
+  json_file = {}
+  def read(self):
+    with open("datos.json") as file:
+      self.json_file = json.load(file)
+    print(self.json_file)
   
-    def read(self):
-        with open("datos.json") as file:
-            self.json_file = json.load(file)
-        print(self.json_file)
-  
-    def write(self):
-        nombre = input ("Nombre: ")
-        email = input ("Email: ")
-    
-        persona = {
-           "nombre":nombre,
-           "email":email
-        }
-
-        self.json_file["personas"].append(persona)
-        with open("datos.json","w") as file:
-          json.dumps(self.json_file,file)
 
 
 objeto = Json()
 objeto.read()
-objeto.write()
-print(objeto.json_file)
+
---
+++ insertar/json/edades.py
@@ -0,0 +1,63 @@
+import web
+import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+urls = (
+    '/datos?', 'Parametros'
+)
+app = web.application(urls, globals())
+
+class Parametros():
+
+  json_file={}
+
+  def GET(self):
+
+        parametros = web.input()
+        action = parametros.action
+
+        if action == "get".replace(" "," "," "):
+            return self.read()
+        elif action == "put".replace(" "," "," "):
+            name = parametros.name
+            fecha_nacimiento = parametros.fecha_nacimiento
+            edad =  parametros.edad
+            return self.write(name,fecha_nacimiento,edad)
+        else:
+            datos = {}
+            datos["status"] = 404 
+            return json.dumps(datos)
+
+
+  def read(self):
+    try:
+      with open("datos.json") as file:
+        self.json_file = json.load(file)
+      print(self.json_file)
+
+    except Exception as error:
+      print("Error {}".format(error.args[0]))
+
+  def write(self,name,fecha_nacimiento,edad):
+    parametros = web.input()
+    name = parametros.name
+    fecha_naci = parametros.fecha_naci
+
+    fecha_nacimiento = datetime.strptime(fecha_naci, "%d-%m-%Y") #//fecha
+    edad = relativedelta(datetime.now(), fecha_nacimiento) #Almacene valores en edad
+    edad = (f"{edad.years} años.")
+    datos = {}
+    datos["status"] = 200
+    datos["nombre"] = name
+    datos["fecha_nacimiento"] = fecha_naci
+    datos["edad"] = edad
+    self.json_file["datos"].append(datos)
+    with open("datos.json","w") as file:json.dump(self.json_file, file)
+ 
+
+   
+
+if __name__ == "__main__":
+  app.run()
